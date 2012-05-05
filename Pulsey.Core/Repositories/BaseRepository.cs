05f3fece18b5f14a6929a7d20f4c5f12f9f6f935
Chris Albrecht <vegashat@gmail.com>
2012-05-05 16:11:46 -0500

    verify database creation works

--- Pulsey.Core/Repositories/BaseRepository.cs
+++ Pulsey.Core/Repositories/BaseRepository.cs
@@ -7,9 +7,18 @@ namespace Pulsey.Core.Repositories
 {
     public class BaseRepository
     {
+        static string _connectionString = null;
+        public BaseRepository(string connectionString = "")
+        {
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                _connectionString = connectionString;
+            }
+        }
+
         public static PulseyContext GetContext()
         {
-            return new PulseyContext();
+            return new PulseyContext(_connectionString);
         }
     }
 }
