b08ab9eac1420259e8090e768df8aad3c72397ea
Chris Albrecht <vegashat@gmail.com>
2012-05-05 13:53:01 -0500

    add core project

---
+++ Pulsey.Core/Models/EventType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulsey.Core.Models
+{
+    public class EventType
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Pulsey.Core/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pulsey.Web.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool AllowEmail { get; set; }
+        public bool AllowSMS { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
