40dd3eb4c31bfa5a5e6d26dddda37c374ea621c8
Chris Albrecht <vegashat@gmail.com>
2012-05-06 08:41:16 -0500

    update solution

---
+++ Pulsey.Core/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Pulsey.Core.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Pulsey.Core.Repositories;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PulseyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PulseyContext context )
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
