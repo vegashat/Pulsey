bf48b0bed2482cbe31a25b328fd3932f3806fc88
Bradley Lower <bratleylower@gmail.com>
2012-07-07 16:37:26 -0500

    add signalr

--- Pulsey.Web/SignalR/PulseyHub.cs
+++ Pulsey.Web/SignalR/PulseyHub.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using SignalR.Hubs;
+using System.Threading.Tasks;
 namespace Pulsey.Web.SignalR
 {
-    public class PulseyHub
+    public class PulseyHub : Hub, IConnected, IDisconnect
     {
+
+
+
+
+
+        public Task Connect()
+        {
+            return Clients.notifyClient("new connection established for " + Context.ConnectionId);
+        }
+
+        public Task Reconnect(IEnumerable<string> groups)
+        {
+            return Clients.notifyClient("connection re-established for " + Context.ConnectionId);
+        }
+
+        public Task Disconnect()
+        {
+            // Tell everyone this connection is gone
+            return Clients.notifyLeave(Context.ConnectionId);
+        }
+
+        public void Send(string message)
+        {
+
+            Clients.addMessage(message);
+
+        }
+
+
     }
 }
