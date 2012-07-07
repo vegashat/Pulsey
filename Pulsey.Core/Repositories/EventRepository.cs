9d0096bbce1a240c899553adb14ffa9f0fe83d50
Matt Chandler <mkchandler@gmail.com>
2012-07-07 16:35:42 -0500

    Added new columns for events and added the event repository.

---
+++ Pulsey.Core/Repositories/EventRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+using Pulsey.Core.Models;
+
+namespace Pulsey.Core.Repositories
+{
+    public class EventRepository : BaseRepository
+    {
+        public ICollection<Event> Get()
+        {
+            using (var context = GetContext())
+            {
+                return context.Events.ToList();
+            }
+        }
+
+        public Event Save(Event model)
+        {
+            using (var context = GetContext())
+            {
+                if (model.Id == 0)
+                {
+                    context.Entry<Event>(model).State = EntityState.Added;
+                }
+                else
+                {
+                    context.Entry<Event>(model).State = EntityState.Modified;
+                }
+
+                context.SaveChanges();
+
+                return model;
+            }
+        }
+    }
+}
