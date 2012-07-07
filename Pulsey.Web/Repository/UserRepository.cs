7348551a812cb95b3cdf7bddfee61e4424745ba2
kartik jambunathan <radiofreak311@gmail.com>
2012-07-07 14:09:19 -0500

    new files

---
+++ Pulsey.Web/Repository/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Pulsey.Core.Models;
+
+namespace Pulsey.Web.Repository
+{
+    public class UserRepository
+    {
+        public User GetUserByEmail(string email)
+        {
+            using (var context = new PulseyContext())
+            {
+                return context.Users.FirstOrDefault(a => a.Email == email);
+            }
+        }
+    }
+}
